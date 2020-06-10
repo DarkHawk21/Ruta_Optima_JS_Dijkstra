77fdfa0317a3e51e84b0afb849e83edfa2aecabb
DarkHawk21 <enriquecarranza-1999@live.com.mx>
2020-06-09 21:21:05 -0500

    Prueba 1

---
+++ js/Dijkstra.js
@@ -0,0 +1,121 @@
+const problema = {};
+
+// Esta función nos genera un mensaje indicando en que paso del algoritmo nos encontramos.
+function log(mensaje) {
+	const imprime = false;
+	
+    if (imprime) {
+        console.log(mensaje);
+    }
+}
+
+// Esta función nos retorna el nodo con menor peso al que podemos acceder.
+const nodoPesoMenor = (pesos, procesado) => {
+    return Object.keys(pesos).reduce((menor, nodo) => {
+        if (menor === null || pesos[nodo] < pesos[menor]) {
+            if (!procesado.includes(nodo)) {
+                menor = nodo;
+            }
+        }
+        return menor;
+    }, null);
+};
+
+// Esta función nos retorna la ruta más barata hasta el nodo final dado el peso de cada arista.
+const dijkstra = (grafo, nodoInicial, nodoFinal) => {
+	// Siguiendo el peso menor para llegar a cada nodo.
+    let pesos = {};
+    pesos[nodoFinal] = "Infinito";
+    pesos = Object.assign(pesos, grafo[nodoInicial]);
+
+    // Siguiendo los caminos / rutas al visitar cada nodo.
+    const nodosPadre = {
+		nodoFinal: null
+	};
+
+    for (let nodoHijo in grafo[nodoInicial]) {
+        nodosPadre[nodoHijo] = nodoInicial;
+    }
+
+    // Almacenando a los nodos que ya han sido procesados.
+    const procesados = [];
+
+    let nodo = nodoPesoMenor(pesos, procesados);
+
+    while (nodo) {
+        let peso = pesos[nodo];
+		let nodosHijo = grafo[nodo];
+		
+        for (let n in nodosHijo) {
+            if (String(n) === String(nodoInicial)) {
+                log("¡No podemos regresar al inicio!");
+            } else {
+                log("Nombre del nodo inicial: " + nodoInicial);
+                log("Evaluando el peso hasta el nodo " + n + " (buscando desde el nodo " + nodo + ")");
+				log("Último peso: " + pesos[n]);
+				
+				let nuevoPeso = peso + nodosHijo[n];
+				
+				log("Nuevo peso: " + nuevoPeso);
+				
+                if (!pesos[n] || pesos[n] > nuevoPeso) {
+                    pesos[n] = nuevoPeso;
+					nodosPadre[n] = nodo;
+					
+                    log("Nodos padre y pesos actualizados.");
+                } else {
+                    log("Ya existe una mejor ruta.");
+                }
+            }
+		}
+		
+        procesados.push(nodo);
+        nodo = nodoPesoMenor(pesos, procesados);
+    }
+
+    let rutaOptima = [nodoFinal];
+	let nodoPadre = nodosPadre[nodoFinal];
+	
+    while (nodoPadre) {
+        rutaOptima.push(nodoPadre);
+        nodoPadre = nodosPadre[nodoPadre];
+	}
+
+	rutaOptima.reverse();
+
+    const resultados = {
+        distancia: pesos[nodoFinal],
+        ruta: rutaOptima
+	};
+
+    return resultados;
+};
+
+problema.vInicial = {
+	A: 5,
+	B: 2
+}
+
+problema.A = {
+	vInicial: 1,
+	C: 4,
+	D: 2
+}
+
+problema.B = {
+	A: 8,
+	D: 7
+}
+
+problema.C = {
+	D: 6,
+	vFinal: 3
+}
+
+problema.D = {
+	vFinal: 1
+}
+
+problema.vFinal = {}
+
+console.log(dijkstra(problema, "vInicial", "vFinal"));
